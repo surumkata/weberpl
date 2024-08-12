3eaf5f20c2817859699a2170a7a9b9708a574061
Tiago Silva <57015073+surumkata@users.noreply.github.com>
2024-08-12 21:41:25 +0100

    bug path fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
       <HashRouter>
           <Routes>
-              <Route path="/weberpl" element={<BlockEditor />} />
+              <Route path="" element={<BlockEditor />} />
               <Route path="/escape_room/:data" element={<EscapeRoomPage />} />
           </Routes>
       </HashRouter>
